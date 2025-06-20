94a27d53b9a4ec375144294e2e494ce1c9817f9a
cyruz <cyruz@duck.com>
2025-06-21 01:25:34 +0800

    finished:iter

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,9 +3,12 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    match num {
+        0|1 => 1,
+        _ => factorial(num - 1) * num,
+    }
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
